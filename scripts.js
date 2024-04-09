332d24d32c1dd3c428049dca9d797aa3ad55e5de
oblivyus-kp <kennypham12323@gmail.com>
2024-04-08 20:55:07 -0700

    Added a searchbar, redesigned the top right of page, and added more items to the catalog

--- scripts.js
+++ scripts.js
@@ -25,15 +25,17 @@ const NEWJEANS_URL = "https://i.pinimg.com/736x/4c/d0/41/4cd041f49b56d42467e6f8c
 const LESS_URL = "https://content.momentica.com/1526a225-1a8e-47c8-85c9-0632cce7399e";
 
 // This is an array of strings 
-// Changed to array of array containing [title, img link]
+// Changed to array of array containing [title, img link, price]
 let titles = [
-    ["BTS","https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/BTS_logo_%282017%29.png/600px-BTS_logo_%282017%29.png?20180426223105"],
-    ["TWICE", "https://pbs.twimg.com/profile_images/1176061001109651456/bR9bAVY3_400x400.jpg"],
-    ["NEWJEANS", "https://i.pinimg.com/736x/4c/d0/41/4cd041f49b56d42467e6f8c1bdd79bed.jpg"], 
-    ["LE SSERAFIM", "https://content.momentica.com/1526a225-1a8e-47c8-85c9-0632cce7399e"]
+    ["BTS","https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/BTS_logo_%282017%29.png/600px-BTS_logo_%282017%29.png?20180426223105", 250],
+    ["TWICE", "https://i.pinimg.com/originals/35/70/51/357051d292c92266ecbe9274052b0814.png", 150],
+    ["NEWJEANS", "https://i.pinimg.com/736x/4c/d0/41/4cd041f49b56d42467e6f8c1bdd79bed.jpg", 175], 
+    ["LE SSERAFIM", "https://content.momentica.com/1526a225-1a8e-47c8-85c9-0632cce7399e", 200],
+    ["Stray Kids", "https://i.redd.it/zs0inteh4sc71.jpg", 100],
+    ["IVE", "https://upload.wikimedia.org/wikipedia/commons/b/bb/Ive_Logo.jpg", 150]
 ];
 
-let deleted = [];
+let deleted = []; //keeps track of removed cards to redisplay later
 
 // you should use more than just an array of strings to store it all.
 
@@ -44,9 +46,12 @@ function showCards() {
     const templateCard = document.querySelector(".card");
     
     for (let i = 0; i < titles.length; i++) {
+
+        //scaled code
         let title = titles[i][0];
         let imageURL = "";
         imageURL = titles[i][1];
+        let price =  titles[i][2];
 
         // This part of the code doesn't scale very well! After you add your
         // own data, you'll need to do something totally different here.
@@ -63,13 +68,14 @@ function showCards() {
         }*/
         
         const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
+        editCardContent(nextCard, title, imageURL, price);
         cardContainer.appendChild(nextCard); // Add new card to the container
     }
     
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+// added a price parameter
+function editCardContent(card, newTitle, newImageURL, price) {
     card.style.display = "block";
 
     const cardHeader = card.querySelector("h2");
@@ -79,17 +85,19 @@ function editCardContent(card, newTitle, newImageURL) {
     cardImage.src = newImageURL;
     cardImage.alt = newTitle + " Poster";
 
+    const cardPrice = card.querySelector(".price");
+    cardPrice.textContent = "Starting at $" + price;
+
     // You can use console.log to help you debug!
     // View the output by right clicking on your website,
     // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+    //console.log("new card:", newTitle, "- html: ", card);
 }
 
-// This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
 function quoteAlert() {
-    console.log("Button Clicked!")
+    //console.log("Button Clicked!")
     alert("PARTY PARTY YEAH!!!!!!!");
 }
 
@@ -117,22 +125,23 @@ function alphabeticalOrder() {
     // temp = [];
 }
 
-function addCard(){
+function addCard(){ // adds a new card to the page
     // group ex: Aespa
     // link ex: https://i.pinimg.com/474x/14/7b/36/147b36a3860593605909d896ca023a92.jpg
     let group = prompt("Enter a Group Name");
     let link = prompt("Enter a link for their photo");
-    titles.push([group, link]);
+    let price = prompt("Enter a starting ticket price");
+    titles.push([group, link, price]);
     showCards();
 }
 
 //revert card container back to original state
 function revert(){
-    while (titles.length > 4){
+    while (titles.length > 6){
         titles.pop();
     }
     for (let i = 0; i < titles.length + 1; i++) {
-        if (titles.length != 4){
+        if (titles.length != 6){
             titles.push(deleted[deleted.length-1-i]);
         } else {
             deleted = [];
@@ -141,3 +150,29 @@ function revert(){
     }
     showCards();
 }
+
+let names = [];
+function titleName(){
+    names = [];
+    for (let i = 0; i < titles.length; i++){
+        names.push(titles[i][0]);
+    }
+}
+
+const searchInput = document.querySelector("[data-search]");
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value.toLowerCase();
+    console.log(value);
+    const cardContainer = document.getElementById("card-container");
+    const cards = cardContainer.children; // Get all card elements
+    for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
+        const cardTitle = card.querySelector("h2").textContent.toLowerCase(); // Get the text content of h2
+        const isVisible = cardTitle.includes(value);
+        if (!isVisible) {
+            card.style.display = "none"; // Hide the card by setting display to none
+        } else {
+            card.style.display = ""; // Show the card by removing inline style (or you can set it to "block" if necessary)
+        }
+    }
+});
